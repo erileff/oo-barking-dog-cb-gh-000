20235428a0bc73398c885eea25afd0b1b5d4e8cf
Erica Leff <ericaleff@gmail.com>
2018-05-12 16:46:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog
-  def name=(name)
-    @name = name
+  def name=(dog_name)
+    this_dogs_name = dog_name
   end
 
   def name
-    @name = name
+    this_dogs_name
   end
 
   def bark
